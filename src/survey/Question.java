ad95658401cb0d7a7485d5d759611b77428e09f6
meta-saloni-khandelwal <35561009+meta-saloni-khandelwal@users.noreply.github.com>
2018-02-09 18:31:33 +0530

    Updated

--- src/survey/Question.java
+++ src/survey/Question.java
@@ -9,18 +9,20 @@ import java.util.ArrayList;
  * @version 1.0
  * @author Saloni Khandelwal
  */
+
 /*
  * This class represents various Questions
  */
 public class Question implements Comparable<Question> {
-	String type;
-	int questionId;
-	String questionHeading;
+	public String type;
+	public int questionId;
+	public String questionHeading;
 	ArrayList<String> listOfOptions = new ArrayList<String>();
-	@Override
-	public int compareTo(Question ques) {
+	
+	public int compareTo(Question question) {
 		// TODO Auto-generated method stub
-		return (this.questionHeading).compareTo(ques.questionHeading);
+		return (this.questionHeading).compareTo(question.questionHeading);
 	}
 	
 }
+
