81db369ee4b4499b7429de56c28866a004b9c571
meta-saloni-khandelwal <35561009+meta-saloni-khandelwal@users.noreply.github.com>
2018-02-09 18:26:25 +0530

    Updated

--- dom/DOMHierarchy.java
+++ dom/DOMHierarchy.java
@@ -17,32 +17,32 @@ import java.util.List;
 public class DOMHierarchy {
 	/**
 	 * Below functions returns the object if id is equal to id if any object in hierarchy.
-	 * @param id
-	 * @param domList
+	 * @param id - stores the id of an element
+	 * @param domList - list having all element of Document Object Model
 	 * @return object of Element if id is found otherwise returns null
 	 */
 	public Element findElementById(String id, List<Element> domList){
-		for(Element i : domList){
-			if(id == i.getId()){
-				return i;														//returns object of Element
+		for(Element element : domList){
+			if(id == element.getId()){
+				return element;					//returns object of Element
 			}
 		}
 		return null;
 	}
 	/**
 	 * Below function returns list of object having class equals to cssClass in hierarchy.
-	 * @param cssClass
-	 * @param domList
+	 * @param cssClass - stores the class of an element
+	 * @param domList - list having all element of Document Object Model
 	 * @return list of objects having same class
 	 */
 	
 	public List<Element> findElementsByClass(String cssClass, List<Element> domList){
-		List<Element> elementsOfClass = new ArrayList<Element>();				//List to store Elements having same class.
-		for(Element i : domList){
-			if(cssClass == i.getCssClassName()){
-				elementsOfClass.add(i);
+		List<Element> elementsOfClass = new ArrayList<Element>();		//List to store Elements having same class.
+		for(Element element : domList){
+			if(cssClass == element.getCssClassName()){
+				elementsOfClass.add(element);
 			}
 		}
-		return elementsOfClass;													//returns final list of Element
+		return elementsOfClass;				//returns final list of Element
 	}
 }
